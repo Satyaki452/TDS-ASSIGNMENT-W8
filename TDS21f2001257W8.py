31c9a8ac1738c9c177cd2542ef9e3a161f927026
Satyaki452 <131292015+Satyaki452@users.noreply.github.com>
2023-04-19 23:25:36 +0530

    Update TDS21f2001257W8.py

--- TDS21f2001257W8.py
+++ TDS21f2001257W8.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Find the largest among the 3 numbers")
-num1 = st.numbe_input ("Enter number 1")
+num1 = st.number_input ("Enter number 1")
 num2 = st.number_input ("Enter number 2")
 num3 = st.number_input ("Enter number 3")
 calculate = st.button("Find largest number")
